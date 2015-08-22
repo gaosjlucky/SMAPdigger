3d4e005ecaea42a68d37f43ad3aee43dc49e32f5
gaosjlucky <gsjlucky@gmail.com>
2015-08-22 21:11:14 +0800

    Update Data_Prepare.sh

--- Data_Prepare.sh
+++ Data_Prepare.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
 perl ./bin/Data_Prepare.pl -I ./data/common/hg19.fa -P CCGG -S 1 -O ./data/common/hg19.fragment.bed -B ./bin
-./bin/bismark_genome_preparation  --bowtie2   --path_to_bowtie ./bin/  --verbose ./data/common/
+./bin/bismark_genome_preparation --bowtie2 --path_to_bowtie $(pwd)/bin/ --verbose ./data/common/
+
